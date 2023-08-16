240dc9c5b593840b3782c38103974a39e55fd3c9
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-08-16 22:20:20 +0300

    Complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ function App() {
         HTML, and also allows us to add in components, which are separate,
         self-contained chunks of JSX.
       </p>
+      
       <ExampleComponent />
     </div>
   );
